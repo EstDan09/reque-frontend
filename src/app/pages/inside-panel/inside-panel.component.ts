7e3c717091051a5036194447b797dfd157994601
Esteban Secaida <esecaidavargas@gmail.com>
2024-08-11 10:34:31 -0600

    initial1.2

---
+++ src/app/pages/inside-panel/inside-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inside-panel',
+  templateUrl: './inside-panel.component.html',
+  styleUrls: ['./inside-panel.component.css']
+})
+export class InsidePanelComponent {
+
+}
